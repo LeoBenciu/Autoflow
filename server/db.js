d1fa3647e27db09dd81b922c19a127229b643ddc
Benciu Leonardo <benciuleonardo@gmail.com>
2025-02-02 21:56:00 +0200

    Add ssl backend

--- server/db.js
+++ server/db.js
@@ -2,12 +2,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://me:password@localhost:5432/api'
-});
-
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
-  process.exit(-1);
+  connectionString: process.env.DATABASE_URL || 'postgres://me:password@localhost:5432/api',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
