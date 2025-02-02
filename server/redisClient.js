625f701449abb129af42173348f96fe1aa5bf3e0
Benciu Leonardo <benciuleonardo@gmail.com>
2025-02-02 12:59:03 +0200

    Update redis client for deploy

--- server/redisClient.js
+++ server/redisClient.js
@@ -1,11 +1,8 @@
 const { createClient } = require('redis');
 
-const redisClient = createClient({
-  socket: {
-    host: 'localhost',
-    port: 6379
-  }
-});
+const redisURL = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const redisClient = createClient({ url: redisURL });
 
 redisClient.on('error', (err) => {
   console.error('Redis Client Error:', err);
